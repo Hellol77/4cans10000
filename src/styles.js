e5e7afbc2e288bdeb0345dd8bcc7d177d344c1bc
Hellol77 <dhe7700@naver.com>
2022-07-27 01:39:30 +0900

    FEAT : 폰트 적용

--- src/styles.js
+++ src/styles.js
@@ -2,14 +2,21 @@ import reset from "styled-reset";
 import styled, { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
 ${reset}
+
     body{ // 폰트 종류, 배경색
+    
         background-color:${(props) =>
-          props.color === true ? "white" : `rgb(243,250,242)`}
+          props.color === true ? "rgb(255,255,255)" : `rgb(243,250,242)`};
+        font-family: 'Noto Sans CJK KR';
+        font-style: normal;
+        font-weight: 500;
+          
     }
     :root{ //색, 폰트 설정, radius 설정
         --button-color:rgb(111 232 151);
         --button-non-color:rgb(113 112 112);
         --input-color:#ddd;
+        --background-color:rgb(255 255 255);
     }
 `;
 
