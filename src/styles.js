6506178df075078494deec8ab1da043ddf327c93
Hellol77 <dhe7700@naver.com>
2022-07-27 22:33:17 +0900

    Fix : styles.js, mainLoading.css, index.html 등 변경

--- src/styles.js
+++ src/styles.js
@@ -10,7 +10,11 @@ ${reset}
         font-family: 'Noto Sans CJK KR';
         font-style: normal;
         font-weight: 500;
-          
+    }
+    nav{
+      background-color:${(props) =>
+        props.color === true ? "rgb(255,255,255)" : `rgb(243,250,242)`};
+    
     }
     :root{ //색, 폰트 설정, radius 설정
         --button-color:rgb(111 232 151);
