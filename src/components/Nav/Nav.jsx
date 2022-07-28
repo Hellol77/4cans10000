e0b6dafe4a545f5b318d7aee86930168dba80a9c
Hellol77 <dhe7700@naver.com>
2022-07-28 22:48:30 +0900

    Fix : Nav 뒤로가기 버튼으로 변경

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import styles from "./nav.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 const Nav = () => {
   return (
     <>
       <div className={styles.main}>
-        <div className={styles.title}>네캔만원</div>
-        <div className={styles.menuBar}>
+        <div className={styles.back}><FontAwesomeIcon icon={faAngleLeft} size="1x"/></div>
+        {/* <div className={styles.menuBar}>
           <FontAwesomeIcon icon={faBars} />
-        </div>
+        </div> */}
       </div>
     </>
   );
