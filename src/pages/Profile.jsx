8928ed7fd6c5b6ab4eae3b37f11ca70ab85c047f
Hellol77 <dhe7700@naver.com>
2022-07-30 22:34:06 +0900

    Feat : Footer.jsx 추가

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Nav from "../components/Nav/Nav";
 import { useNavigate } from "react-router-dom";
+import ProfileBox from "../components/profileBox/ProfileBox";
+import Footer from "../components/Footer/Footer";
 const Profile = () => {
   let navigate = useNavigate();
   return (
     <>
-      <Nav navigate={navigate} />
+      <div>
+        <Nav navigate={navigate} />
+        <ProfileBox />
+      </div>
+      <Footer />
     </>
   );
 };
