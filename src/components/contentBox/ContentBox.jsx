8b7a978ce2ffea84d45ad2cf545f893a7704aab2
Hellol77 <dhe7700@naver.com>
2022-08-03 03:30:21 +0900

    Feat : contentBox

---
+++ src/components/contentBox/ContentBox.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./contentBox.module.css";
+const ContentBox = () => {
+  return (
+    <>
+      <div className={styles.mainBox}></div>
+    </>
+  );
+};
+
+export default ContentBox;
