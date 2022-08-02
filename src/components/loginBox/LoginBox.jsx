479ca9babae6bd04ca8a3ca30d4afa98c3089b51
Hellol77 <dhe7700@naver.com>
2022-08-02 22:01:35 +0900

    Feat : 생년월일, 중복검사 버튼 스타일

--- src/components/loginBox/LoginBox.jsx
+++ src/components/loginBox/LoginBox.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import styles from "./loginBox.module.css";
+
 const LoginBox = () => {
   const [loginState, setLoginState] = useState(true);
   const onClickLoginButton = (e) => {
@@ -70,16 +71,66 @@ const LoginBox = () => {
               <button className={styles.Button} onClick={onClickLoginButton}>
                 로그인
               </button>
-              <label className={styles.label}>이메일</label>
-              <button>중복검사</button>
-              <input type="email" className={styles.loginInput} name='email'/>
+              <div className={styles.emailCheckDiv}>
+                <label className={styles.label}>이메일</label>
+                <button className={styles.emailCheck}>중복검사</button>
+              </div>
+              <input type="email" className={styles.loginInput} name="email" />
               <label className={styles.label}>닉네임</label>
-              <input type="text" className={styles.loginInput} name='nickname'/>
-              
+              <input
+                type="text"
+                className={styles.loginInput}
+                name="nickname"
+              />
+              <label className={styles.label}>생년월일</label>
+              <div className={styles.birthBox}>
+                <input
+                  type="number"
+                  className={styles.birthBoxInput}
+                  name="year"
+                  min="1000"
+                  max="9999"
+                  placeholder="년(4자)"
+                />
+                <select
+                  className={styles.birthBoxInput}
+                  aria-label="월"
+                  name="month"
+                >
+                  <option>월</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10</option>
+                  <option value="11">11</option>
+                  <option value="12">12</option>
+                </select>
+                <input
+                  type="text"
+                  className={styles.birthBoxInput}
+                  name="day"
+                  maxLength="2"
+                  placeholder="일"
+                />
+              </div>
               <label className={styles.label}>비밀번호</label>
-              <input type="password" className={styles.loginInput} name='password'/>
+              <input
+                type="password"
+                className={styles.loginInput}
+                name="password"
+              />
               <label className={styles.label}>비밀번호 확인</label>
-              <input type="password" className={styles.loginInput} name='passwordCheck'/>
+              <input
+                type="password"
+                className={styles.loginInput}
+                name="passwordCheck"
+              />
               <button className={styles.submitButton} type="submit">
                 회원가입
               </button>
