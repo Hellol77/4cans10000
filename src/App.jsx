b0df80c91c2881df989c03bc80593c9f40a541b4
Hellol77 <dhe7700@naver.com>
2022-08-03 03:35:16 +0900

    Fix : 기타 파일들 설정

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Login from "./pages/Login";
 import Menu from "./pages/Menu";
 import Profile from "./pages/Profile";
 import { GlobalStyle } from "./styles";
+import Like from "./pages/Like";
+import Random from "./pages/Random";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/menu" element={<Menu />} />
+        <Route path="/like" element={<Like />} />
+        <Route path="/random" element={<Random />} />
       </Routes>
     </>
   );
