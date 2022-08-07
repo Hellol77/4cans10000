0b7f86623c231054a1421e76cc49c13da173c4d6
Chaevivin <chaevivin@gmail.com>
2022-08-07 23:38:15 +0900

    Search 페이지 수정

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import BeerList from '../components/beerList/beerList';
+import BeerSearch from '../components/beerSearch/beerSearch';
 import FilterBox from './../components/filterBox/filterBox';
 
 const Search = (props) => {
     return (
         <div>
-            <BeerList />
+            <BeerSearch />
             <FilterBox />
             <BeerList />
+            <Footer />
         </div>
     );
 };
